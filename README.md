2ae3f18b79f20831286fb84d18a7b2dce2f173ae
bluescorpian <dsharryh27@gmail.com>
2024-10-31 15:11:47 +0200

    update documentation

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 ![Deno JS](https://img.shields.io/badge/deno%20js-000000?style=for-the-badge&logo=deno&logoColor=white)
 [![JSR](https://jsr.io/badges/@bluescorpian/tough-cookie-deno-kv-store)](https://jsr.io/@bluescorpian/tough-cookie-deno-kv-store)
 [![JSR Score](https://jsr.io/badges/@bluescorpian/tough-cookie-deno-kv-store/score)](https://jsr.io/@bluescorpian/tough-cookie-deno-kv-store)
-
 [![codecov.io Code Coverage](https://img.shields.io/codecov/c/github/dwyl/hapi-auth-jwt2.svg?maxAge=2592000)](store_test.ts)
 
 `tough-cookie-deno-kv-store` is a Deno KV store adapter for the `tough-cookie` library. This adapter allows you to store and manage cookies using Deno's key-value store.
--- store.ts
+++ store.ts
@@ -8,11 +8,33 @@ import {
 	pathMatch,
 } from "tough-cookie";
 
+/**
+ * DenoKVStore class for handling cookie storage within Deno's key-value store.
+ * Extends the base {@link Store} class.
+ *
+ * This store can be used asynchronously and implements all required cookie
+ * management methods, including find, remove, and update cookies.
+ *
+ * @public
+ */
 export class DenoKVStore extends Store {
 	constructor(private kv: Deno.Kv, private prefix: string[]) {
 		super();
 		this.synchronous = false;
 	}
+
+	/**
+	 * Retrieve a {@link Cookie} with the given `domain`, `path`, and `key` (`name`).
+	 *
+	 * The RFC maintains that exactly one of these cookies should exist in a store.
+	 * If the store is using versioning, this means that the latest or newest such cookie
+	 * should be returned.
+	 *
+	 * @param domain - The cookie domain to match against.
+	 * @param path - The cookie path to match against.
+	 * @param key - The cookie name to match against.
+	 * @returns The matching cookie, or `undefined` if none is found.
+	 */
 	override async findCookie(
 		domain: Nullable<string>,
 		path: Nullable<string>,
@@ -22,10 +44,22 @@ export class DenoKVStore extends Store {
 			return undefined;
 		}
 		const result = await this.kv.get(
-			this.prefix.concat([domain, path, key]) // idk how I am suppose to handle null values
+			this.prefix.concat([domain, path, key])
 		);
 		return result.value ? Cookie.fromJSON(result.value) : undefined;
 	}
+
+	/**
+	 * Locates all {@link Cookie} values matching the given `domain` and `path`.
+	 *
+	 * The resulting list is checked for applicability to the current request
+	 * according to the RFC (`domain-match`, `path-match`, `http-only-flag`, `secure-flag`, `expiry`, and so on).
+	 *
+	 * @param domain - The cookie domain to match against.
+	 * @param path - The cookie path to match against. If `null`, retrieves cookies for all paths.
+	 * @param allowSpecialUseDomain - If `true`, special-use domain suffixes will be allowed in matches.
+	 * @returns Array of matching cookies.
+	 */
 	override async findCookies(
 		domain: Nullable<string>,
 		path: Nullable<string>,
@@ -40,6 +74,7 @@ export class DenoKVStore extends Store {
 		let pathMatcher: (
 			domainIndex: Deno.KvListIterator<CreateCookieOptions>
 		) => Promise<void>;
+
 		if (!path) {
 			// null means "all paths"
 			pathMatcher = async function matchAll(domainIndex): Promise<void> {
@@ -51,8 +86,6 @@ export class DenoKVStore extends Store {
 			};
 		} else {
 			pathMatcher = async (domainIndex): Promise<void> => {
-				// NOTE: we should use path-match algorithm from S5.1.4 here
-				// (see: https://github.com/ChromiumWebApps/chromium/blob/b3d3b4da8bb94c1b2e061600df106d590fda3620/net/cookies/canonical_cookie.cc#L299)
 				for await (const entry of domainIndex) {
 					const cookiePath = entry.key
 						.slice(this.prefix.length)[1]
@@ -76,6 +109,15 @@ export class DenoKVStore extends Store {
 
 		return results;
 	}
+
+	/**
+	 * Gets all the cookies in the store.
+	 *
+	 * @remarks
+	 * - Cookies SHOULD be returned in creation order to preserve sorting.
+	 *
+	 * @returns An array of all stored cookies.
+	 */
 	override async getAllCookies(): Promise<Cookie[]> {
 		const cookies: Cookie[] = [];
 
@@ -90,27 +132,43 @@ export class DenoKVStore extends Store {
 		}
 		return cookies;
 	}
+
+	/**
+	 * Adds a new {@link Cookie} to the store. The implementation replaces any existing cookie with the same `domain`,
+	 * `path`, and `key` properties.
+	 *
+	 * @param cookie - The cookie to store.
+	 */
 	override async putCookie(cookie: Cookie): Promise<void> {
 		const { domain, path, key } = cookie;
-		// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 		if (domain == null || path == null || key == null) {
 			return undefined;
 		}
 
-		// deno-lint-ignore no-unused-vars
-		const result = await this.kv.set(
+		await this.kv.set(
 			this.prefix.concat([domain, path, key]),
 			cookie.toJSON()
-		); // TODO: handle not ok result
-
+		);
 		return undefined;
 	}
+
+	/**
+	 * Removes all cookies from the store.
+	 */
 	override async removeAllCookies(): Promise<void> {
 		const entries = this.kv.list<SerializedCookie>({ prefix: this.prefix });
 		for await (const entry of entries) {
 			await this.kv.delete(entry.key);
 		}
 	}
+
+	/**
+	 * Remove a cookie from the store.
+	 *
+	 * @param domain - The cookie domain to match against.
+	 * @param path - The cookie path to match against.
+	 * @param key - The cookie name to match against.
+	 */
 	override async removeCookie(
 		domain: string,
 		path: string,
@@ -118,6 +176,13 @@ export class DenoKVStore extends Store {
 	): Promise<void> {
 		await this.kv.delete(this.prefix.concat([domain, path, key]));
 	}
+
+	/**
+	 * Removes matching cookies from the store. If `path` is omitted, all paths in a domain will be removed.
+	 *
+	 * @param domain - The cookie domain to match against.
+	 * @param path - The cookie path to match against.
+	 */
 	override async removeCookies(domain: string, path: string): Promise<void> {
 		const entries = this.kv.list<SerializedCookie>({
 			prefix: this.prefix.concat([domain, path]),
@@ -126,11 +191,19 @@ export class DenoKVStore extends Store {
 			await this.kv.delete(entry.key);
 		}
 	}
+
+	/**
+	 * Update an existing {@link Cookie}. Replaces the `value` for a cookie with the same `domain`,
+	 * `path`, and `key`.
+	 *
+	 * @param _oldCookie - The existing cookie in the store.
+	 * @param newCookie - The new cookie replacing the existing one.
+	 */
 	override async updateCookie(
 		_oldCookie: Cookie,
 		newCookie: Cookie
 	): Promise<void> {
-		await this.putCookie(newCookie); // TODO: check if old value is equal to new value
+		await this.putCookie(newCookie);
 	}
 }
 
